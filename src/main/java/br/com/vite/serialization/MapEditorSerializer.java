5fa2b21d081b837563e4632b5e35d909f0ceae24
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-03 01:46:38 -0300

    Serializer working

--- src/main/java/br/com/vite/serialization/MapEditorSerializer.java
+++ src/main/java/br/com/vite/serialization/MapEditorSerializer.java
@@ -3,7 +3,9 @@ package br.com.vite.serialization;
 import java.lang.reflect.Type;
 
 import br.com.vite.editor.MapEditor;
+import br.com.vite.tile.Tile;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
@@ -15,14 +17,54 @@ public class MapEditorSerializer implements JsonSerializer<MapEditor> {
 	public JsonElement serialize(MapEditor editor, Type type,
 			JsonSerializationContext context) {
 
+		final int lines = editor.getLines();
+		final int columns = editor.getColumns();
+		
 		JsonObject element = new JsonObject();
 		
-		element.add("columns", context.serialize(editor.getColumns()));
-		element.add("lines", context.serialize(editor.getLines()));		
+		element.add("columns", context.serialize(columns));
+		element.add("lines", context.serialize(lines));		
 		element.add("tile_width", context.serialize(editor.getTileWidth()));
-		element.add("tile_height", context.serialize(editor.getTileHeight()));		
+		element.add("tile_height", context.serialize(editor.getTileHeight()));
 		
+		JsonArray array = serializeTileArray(editor.getTiles());
+
+		//Set<String> tilesets = new HashSet<String>();
+		
+		element.add("tiles", array);
+
 		return element;
+	}
+	
+	private JsonArray serializeTileArray(Tile[][] tiles) {
+		
+		int columns = tiles[0].length;
+		int lines = tiles.length;
+		
+		JsonArray array = new JsonArray();
+
+		for(int j = 0; j < lines; j++) {
+			
+			for(int i = 0; i < columns; i++) {
+				
+				Tile tile = tiles[j][i];
+				
+				if(tile.getLayer() == null)
+					continue;
+								
+				JsonObject tileNode = new JsonObject();
+				
+				tileNode.addProperty("tileSet", tile.getLayer().getPath());
+				tileNode.addProperty("x", i);
+				tileNode.addProperty("y", j);
+				tileNode.addProperty("xImage", tile.getLayer().getX());
+				tileNode.addProperty("yImage", tile.getLayer().getY());
+				
+				array.add(tileNode);
+			}
+		}
+		
+		return array;
 	}
 
 }
