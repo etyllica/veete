b65cbdb9a3ab346211091f0b8d356e039d2e1f77
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-03 01:40:38 -0300

    Fix assets path

--- src/main/java/ViteMapEditor.java
+++ src/main/java/ViteMapEditor.java
@@ -18,7 +18,7 @@ public class ViteMapEditor extends EtyllicaFrame {
 	public Application startApplication() {
 		
 		String s = ViteMapEditor.class.getResource("").toString();
-		setPath(s+"../../../bin/");
+		setPath(s+"../../../");
 			
 		//return new IsometricMapApplication(w, h);
 		return new OrthogonalMapApplication(w, h);
