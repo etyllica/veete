bdc49e9bb4983b98d30db4a2759ffc6ae91dc9d7
Yuri Pourre <yuriamayuri@gmail.com>
2014-09-12 12:30:55 -0300

    Draw procedural collision map

--- src/main/java/br/com/vite/tile/collision/CollisionTileSet.java
+++ src/main/java/br/com/vite/tile/collision/CollisionTileSet.java
@@ -2,6 +2,7 @@ package br.com.vite.tile.collision;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 
 import br.com.vite.tile.set.ProceduralTileSet;
 
@@ -20,10 +21,17 @@ public class CollisionTileSet extends ProceduralTileSet {
 		Graphics2D g = layer.getGraphics();
 		
 		g.setColor(color);
-		g.fillRect(0, 0, tileWidth, tileHeight);
-		
-		g.setColor(Color.GREEN);
 		g.fillRect(tileWidth, 0, tileWidth, tileHeight);
+		
+		Polygon polygon = new Polygon();
+		polygon.addPoint(tileWidth, tileHeight);
+		polygon.addPoint(0, tileHeight*2);
+		polygon.addPoint(tileWidth, tileHeight*3);
+		polygon.addPoint(tileWidth*2, tileHeight*2);
+		
+		g.fillPolygon(polygon);		
+		
+		g.fillRoundRect(tileWidth*2, 0, tileWidth*3, tileHeight*3, tileWidth, tileHeight);
 	}
 
 }
